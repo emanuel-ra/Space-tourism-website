e31d810529cfd24b1ac9c4fe028f388e478a82c6
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2022-10-24 18:24:51 -0500

    destionation page in progress

--- src/Destination/Body.jsx
+++ src/Destination/Body.jsx
@@ -3,11 +3,13 @@ import SliderDestination from "../components/SliderDestination"
 function Body() {
   
   return (
-    <section className="w-full">
-        <article className="container m-auto">
+    <section className="w-full mt-48 px-48">
+        <article className="">
             <h2 className="text-3xl"><span className="text-gray-500">01</span> <span className="text-white">PICK YOUR DESTINATION</span></h2>
         </article>                
-       <SliderDestination />
+       <article className="w-[90%]">
+        <SliderDestination />
+       </article>
     </section>
   )
 }
--- src/components/SliderDestination.jsx
+++ src/components/SliderDestination.jsx
@@ -1,21 +1,50 @@
-import {data} from "../data/data"
+import {useState} from 'react'
+import { data } from "../data/data";
+
+
 
 function SliderDestination() {
-    let destinations = data.destinations;
-
-
-  return (    
+  let destinations = data.destinations;
+  const [destination, setDestination] = new useState(0);
+  return (
     <>
-        <section className="bg-white">
-            {destinations.map( (item,index) => (( 
-                                               
-                <article className="" key={index}>
-                    <img src={new URL(item.images.img, import.meta.url).href} alt={item.name} />
+      <section className={`flex flex-wrap p-12`}>
+        {destinations.map((item, index) => (
+            
+          <article className={`${destination!=index && 'hidden'} ${destination}`} key={index}>
+            <img
+              src={new URL(item.images.img, import.meta.url).href}
+              alt={item.name}
+            />
+
+            <div className="flex flex-col text-white">
+              <ul className="flex space-x-2">
+                {destinations.map((option, i) => (
+                  <li key={i}>
+                    <button onClick={()=>{setDestination(i)}}>{option.name}</button>
+                  </li>
+                ))}
+              </ul>
+              <blockquote>{item.description}</blockquote>
+              <span className="w-full border-b-[0.1rem] border-gray-500"></span>
+
+              <section className="flex">
+                <article>
+                  <span className="uppercase">AVG. DISTANCE</span>
+                  <span>{item.distance}</span>
                 </article>
-            )))}
-        </section>
+
+                <article>
+                  <span className="uppercase">EST. TRAVEL TIME</span>
+                  <span>{item.travel}</span>
+                </article>
+              </section>
+            </div>
+          </article>          
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default SliderDestination
+export default SliderDestination;
